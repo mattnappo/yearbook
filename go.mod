a02ed25bdc87086184905b412c1084cb219e3757
xoreo <mattnappo@gmail.com>
2020-04-08 21:01:09 -0400

    main: Got the core DB functionality working.

--- go.mod
+++ go.mod
@@ -5,6 +5,4 @@ go 1.14
 require (
 	github.com/go-pg/pg/v9 v9.1.5
 	golang.org/x/crypto v0.0.0-20200221231518-2aa609cf4a9d
-	golang.org/x/sys v0.0.0-20191010194322-b09406accb47 // indirect
-	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
--- main.go
+++ main.go
@@ -1,30 +1,51 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
 	"github.com/go-pg/pg/v9"
 	"github.com/go-pg/pg/v9/orm"
 	"golang.org/x/crypto/ssh/terminal"
+	"strings"
+	"time"
 )
 
+const emailPrefix = "@mastersny.org"
+
+type PostID string
+
 type User struct {
-	ID           int64  `sql:"notnull"`
-	FirstName    string `sql:"notnull"`
-	LastName     string `sql:"notnull"`
-	Username     string `sql:"notnull"`
-	Email        string `sql:"notnull"`
-	RegisterDate string `sql:"notnull"`
+	ID           string `pg:",notnull"`
+	Firstname    string `pg:",notnull"`
+	Lastname     string `pg:",notnull"`
+	Username     string `pg:",notnull"`
+	Email        string `pg:",notnull"`
+	RegisterDate string `pg:",notnull"`
+}
+
+func NewUserFromEmail(email string) (*User, error) {
+	if email[len(email)-len(emailPrefix):] == emailPrefix {
+		userData := strings.Split(email, ".")
+		return &User{
+			ID:           "hash of email",
+			Firstname:    userData[0],
+			Lastname:     userData[1],
+			Username:     email[0 : len(email)-len(emailPrefix)],
+			Email:        email,
+			RegisterDate: time.Now().String(),
+		}, nil
+	}
+	return nil, errors.New("invalid email address")
 }
 
 type Post struct {
-	PostID     PostID `sql:"notnull"`
-	Timestamp  string `sql:"notnull"`
-	Sender     User   `sql:"notnull"`
-	Recipients []User `sql:"notnull"`
+	PostID     PostID  `pg:",notnull"`
+	Timestamp  string  `pg:",notnull"`
+	Sender     *User   `pg:",notnull"`
+	Recipients []*User `pg:",notnull"`
 
-	Message string `sql:"notnull"`
-	Images  [][]byte
+	Message string   `pg:",notnull"`
+	Images  [][]byte `pg:",notnull"`
 }
 
 func Connect() *pg.DB {
@@ -34,19 +55,41 @@ func Connect() *pg.DB {
 	db := pg.Connect(&pg.Options{
 		User:     "postgres",
 		Password: string(pwd),
+		Database: "gotests",
 	})
 	return db
 }
 
-func Model() error {
+func Test(shouldCreateSchema bool) error {
 	db := Connect()
 	defer db.Close()
 
-	err := createSchema(db)
+	if shouldCreateSchema {
+		err := createSchema(db)
+		if err != nil {
+			return err
+		}
+	}
+
+	user1, err := NewUserFromEmail("first1.last1@mastersny.org")
 	if err != nil {
 		return err
 	}
-
+	user2, err := NewUserFromEmail("first2.last2@mastersny.org")
+	user3, err := NewUserFromEmail("first3.last3@mastersny.org")
+	testPost := &Post{
+		PostID:     PostID("some_post_id"),
+		Timestamp:  "I'm the time!",
+		Sender:     user1,
+		Recipients: []*User{user2, user3},
+		Message:    "message message message text",
+		Images:     [][]byte{[]byte("img1"), []byte("img2")},
+	}
+	err = db.Insert(testPost)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func createSchema(db *pg.DB) error {
@@ -62,5 +105,8 @@ func createSchema(db *pg.DB) error {
 }
 
 func main() {
-	Model()
+	err := Test(true)
+	if err != nil {
+		panic(err)
+	}
 }
