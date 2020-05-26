7ce54b16e5582ebc3ee1fbea7ac163ec1ac4cf42
xoreo-server <mattnappo@gmail.com>
2020-05-26 00:53:20 +0000

    database: Removed charlie hack.

--- database/database.go
+++ database/database.go
@@ -95,10 +95,6 @@ func (db *Database) AddSeniors() error {
 			return err
 		}
 
-		if senior != "charlie.loigman@mastersny.org" {
-			continue
-		}
-
 		// Add the senior to the database
 		err = db.AddUser(seniorAccount)
 		if err != nil {
